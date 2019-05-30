0775a956dc966ff0f1adcd19719a2deddbf5409b
nitrogendragon <dirtboy4fun@gmail.com>
2019-05-29 20:37:39 -0500

    added tile map with some colliders

---
+++ SDL2_Game_Engine/Assets/colliders_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="colliders_32x32" tilewidth="32" tileheight="32" tilecount="40" columns="10">
+ <image source="colliders_32x32.png" trans="aaaa00" width="320" height="128"/>
+</tileset>
